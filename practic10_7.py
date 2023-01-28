47dfe5fcb361a4e94f16b86ad70f416d83bac5ba
DmitrievRB <merlinrd@yandex.ru>
2023-01-28 18:49:34 +0300

    первичный вариант практики 12.7.3

--- practic10_7.py
+++ practic10_7.py
@@ -1,9 +1,7 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = float(input(("Введите сумму вклада:")))
+money = float(input("Введите сумму вклада:"))
 
-deposit =[money/100*i for i in per_cent.values()]
+deposit = [money / 100 * i for i in per_cent.values()]
 
 print(deposit)
-print("Максимальная сумма, которую вы можете заработать",(round(max(deposit)),2))
-
-
+print(" Максимальная сумма, которую вы можете заработать ", (round(max(deposit), 2)))
