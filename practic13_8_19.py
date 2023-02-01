12e181938f85e7a9d80914a9870aef9bf5ef5edc
DmitrievRB <merlinrd@yandex.ru>
2023-02-02 00:13:23 +0300

    Update comment practic13_8_19.py

--- practic13_8_19.py
+++ practic13_8_19.py
@@ -3,7 +3,7 @@ try:
 except ValueError:
     count_people = int(0)
 finally:
-    print("Мы признательны за участие")  # Обработка ввода пустой сторки и сторчных символов
+    print("Спасибо за участие в конференции")  # Обработка ввода пустой сторки и сторчных символов
 age_people = []
 minors = []
 junior = []
@@ -12,9 +12,9 @@ price_minors = 0  # цена детского билета
 price_junior = 990  # цена молодежного билета
 price_adults = 1390  # цена взрослого билета
 while len(age_people) < count_people:
-    age_people.append(int(input("Введите возраст")))  # Проводим опрос о возрасте и наполняем список заказа
+    age_people.append(int(input("Возраст участника конференции")))  # Проводим опрос о возрасте и наполняем список заказа
 else:
-    print("Ваш заказ:")
+    print("Ваша бронь:")
 for age in age_people:
     if age < 18:
         minors.append(price_minors)  # Обробатываем посетителей детского тарифа
@@ -24,13 +24,13 @@ for age in age_people:
         adults.append(price_adults)  # Обробатываем посетителей взрослого тарифа
         # Исходя из бронирования по возрастам формируем чек
 if len(minors) > 0:
-    print(str(len(minors)) + "  детских  цена        " + str(price_minors) + " руб")
+    print("  детских          "+str(len(minors)) +"шт  "+ "  цена " +  str(price_minors) + " руб")
 if len(junior) > 0:
-    print(str(len(junior)) + "  молодежных цена " + str(price_junior) + " руб")
+    print("  молодежных "+str( len(junior)) +"шт  "+"  цена " + str(price_junior) + " руб")
 if len(adults) > 0:
-    print(str(len(adults)) + "  взрослых цена      " + str(price_adults) + " руб")
+    print("  взрослых       " +str( (len(adults))) +"шт  "+"  цена " + str(price_adults) + " руб")
 
-print("Итого со скидкой  " + str((sum(junior) + sum(adults)) * 0.9) + "  руб" if sum(junior) + sum(
+print("Итого со скидкой 10% " + str((sum(junior) + sum(adults)) * 0.9) + "  руб" if sum(junior) + sum(
     adults) >= 2970 else "Итого:" + str(sum(junior) + sum(adults)))
 # Расчет скидки  осуществляется если есть 3 оплачиваемых билета, в нашем \
 # случае минимальная сумма такого короитерия это 3 билета молодежного \
